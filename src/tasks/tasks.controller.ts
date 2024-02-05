f8f36c78bd83b89a8df2504fcee4c9a4cf82b482
majidalykhan <majidalykhan@gmail.com>
2024-02-05 20:12:03 +0500

    feat: guard tasks
    protect tasks so only logged in users can access

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -8,17 +8,20 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { TaskStatusValidationPipe } from '../common/pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
 import { TaskStatus } from './enum/task-status.enum';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
