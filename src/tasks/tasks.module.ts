0773fb316b09c1eda31c3120a8bd9c78e5b893da
majidalykhan <majidalykhan@gmail.com>
2024-02-05 13:11:19 +0500

    feat: auth module
    add controller, service, user entity and repository for auth module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from './task.entity';
 import { TaskRepository } from './task.repository';
 
 @Module({
